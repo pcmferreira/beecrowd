a25ad225e10aa80754a9ab6de444fe6959dec496
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2021-08-28 22:58:08 -0300

    1190.cpp

---
+++ solutions/1-beginner/1190.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    const int n = 12;
+    char o; cin >> o;
+    double sum = 0;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            double x; cin >> x;
+            if (j > i && i + j >= n) {
+                sum += x;
+                ++cnt;
+            }
+        }
+    }
+    if (o == 'M') sum /= cnt;
+    cout << fixed << setprecision(1) << sum << endl;
+    return 0;
+}
