b383e32332482828a39fe6c5254cd775a6a672aa
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2021-09-08 01:59:55 -0300

    1001.cs

---
+++ solutions/1-beginner/1001.cs
@@ -0,0 +1,11 @@
+using System; 
+ 
+class URI {
+
+    static void Main(string[] args) { 
+        int a = Int32.Parse(Console.ReadLine());
+        int b = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("X = {0}", a + b);
+    }
+
+}
