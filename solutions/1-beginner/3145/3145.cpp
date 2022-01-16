64625b3a061eee603e670948b534b5a54b2f2ba1
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2022-01-15 22:04:49 -0300

    3145.cpp

---
+++ solutions/1-beginner/3145/3145.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N, X; cin >> N >> X;
+    cout << fixed << setprecision(2) << ((double) X / (N + 2)) << endl;
+    return 0;
+}
