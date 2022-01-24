d5f3fcbcd1c0d5f46cdb15a9fd816449a624a833
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2022-01-24 18:20:37 -0300

    2451-a.cpp

--- solutions/2-ad-hoc/2451/2451-a.cpp
+++ solutions/2-ad-hoc/2451/2451-a.cpp
@@ -7,19 +7,19 @@ int main() {
     vector<string> board(n);
     for (int i = 0; i < n; ++i) cin >> board[i];
     int ans = 0;
-    int food = 0;
+    int score = 0;
     for (int i = 0; i < n; ++i) {
         if (i % 2 == 0) {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] == 'o') {
-                    if (++food > ans) ans = food;
-                } else if (board[i][j] == 'A') food = 0;
+                    if (++score > ans) ans = score;
+                } else if (board[i][j] == 'A') score = 0;
             }
         } else {
             for (int j = n - 1; j >= 0; --j) {
                 if (board[i][j] == 'o') {
-                    if (++food > ans) ans = food;
-                } else if (board[i][j] == 'A') food = 0;
+                    if (++score > ans) ans = score;
+                } else if (board[i][j] == 'A') score = 0;
             }
         }
     }
